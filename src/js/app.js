1233444b442caa2173412732f8c42822dc78723c
Abhay Vincent <abhaykvincent@gmail.com>
2020-02-06 18:57:29 -0500

    Login Screen Functional
    profile click and pop-up
    back button functional

--- src/js/app.js
+++ src/js/app.js
@@ -16,10 +16,45 @@ $(document).ready(function () {
         image: "guest"
     },];
 
-
     //showing accounts screen
     $(".accounts-screen").show();
+    $(".acctPassword").hide();
+    $(".acctDetails").hide();
 
+    // setting accounts name
     $(".accountName").text(accounts[0].name);
-    console.log(accounts[0].name)
+
+    //onclick background blur & make account profile center and password input
+    let thisDiv;
+    $(".accounts").click(function (e) { 
+        e.preventDefault();
+        thisDiv=this;
+        //display back button
+        $(".back").css({"opacity":"1"});
+        //hide all accounts
+        $(".accounts").not(this).hide();
+        //blur background
+
+        //make selected account center to screen
+        $(this).css({"left": "50vw","transform": "translate(-50%,-50%)"});
+        //show input box for password
+        $(this).find(".acctPassword").show();
+        $(this).find(".acctDetails").show();
+        $(this).find(".accountName").hide();
+        $('.acctPassword').focus();
+    });
+
+    $(".back").click(function (e) { 
+        e.preventDefault();
+        //hide all accounts
+        $(".accounts").not(this).show();
+        //blur background
+
+        //make selected account center to screen
+        $(thisDiv).removeAttr('style'); 
+        //show input box for password
+        $(thisDiv).find(".acctPassword").hide();
+        $(thisDiv).find(".acctDetails").hide();
+        $(thisDiv).find(".accountName").show();
+    });
 });
