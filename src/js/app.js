466981ead96d0f77e424a38751ad7ba772683391
Abhay Vincent <abhaykvincent@gmail.com>
2020-02-18 12:13:06 -0500

    landing structure upodated

--- src/js/app.js
+++ src/js/app.js
@@ -16,7 +16,6 @@ $(document).ready(function () {
         type:"guest",
         image: "guest"
     },];
-
     let login={
         accountName:"Abhay Vincent",
         isLoggedIn: true
@@ -83,9 +82,91 @@ $(document).ready(function () {
     // Landing Screen   //
     //////////////////////
 
+
+//////////////
+var leftPanel=[
+    {
+        name: "Blog",
+        icon:`<i class="fas fa-newspaper"></i>`,
+        options:["Add post","Edit post"],
+        optionIcon:[`<i class="fas fa-plus"></i>`,`<i class="far fa-trash-alt"></i>`]
+    },
+    {
+        name: "Apeiro",
+        icon:`<i class="fab fa-battle-net"></i>`,
+        options:["Add post","Add post","Add post"],
+        optionIcon:[`<i class="fas fa-plus"></i>`,`<i class="far fa-trash-alt"></i>`],
+    },
+    {
+        name: "Settings",
+        icon:`<i class="fas fa-sliders-h"></i>`,
+        options:["Change Password","Add post","Logout"],
+        optionIcon:[`<i class="fas fa-plus"></i>`,`<i class="far fa-trash-alt"></i>`],
+    }
+];
+var topPanel=[
+    {name:"Lorem",
+    options:["Lorem","ipsum", "dolor", "sit", "amet"]},
+    {name:"Lorem",
+    options:["Lorem","ipsum", "dolor", "dolor", "sit", "amet", "dolor", "sit", "amet"]},
+    {name:"Lorem",
+    options:["Lorem","ipsum", "dolor", "sit", "amet"]},
+];
+var tasks=[{
+        taskId:1,
+        taskName:  "Portfolio Development",
+        status: "pending"
+    },{
+        taskId:2,
+        taskName:  "Easy Grocries",
+        status: "pending"
+    },{
+        taskId:3,
+        taskName:  "Resume",
+        status: "pending"
+    },{
+        taskId:4,
+        taskName:  "Regex Form",
+        status: "pending"
+    },{
+        taskId:5,
+        taskName:  "Wordpress",
+        status: "pending"
+}];
+var postSettings={
+    options:[
+    {
+        name:"Edit Post",
+        icon:`<i class="fas fa-edit"></i>`
+    },{
+        name:"Delete Post",
+        icon:`<i class="fas fa-trash-alt"></i>`
+    },{
+        name:"Private",
+        icon:`<i class="fas fa-user-lock"></i>`
+    }]
+}
+var posts=[
+    {
+        location: "Oshawa, Canada",
+        date: "10 Jan 2020",
+        
+        heading: "Lorem ipsum dolor sit amet ",
+        subHeading: "consectetur adipisicing elit. Quidem, sit, nam autem quas incidunt",
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto rem quasi magnam possimus doloribus nobis neque non deserunt explicabo. Numquam autem sint quasi corporis quas iste provident iusto dolorem doloribus.",
+    },
+    {
+        location: "Oshawa, Canada",
+        date: "11 Jan 2020",
+        
+        heading: "Lorem ipsum dolor  ",
+        subHeading: "consectetur adipisicing elit. Quidem, sit, nam autem quas incidunt sit amet sit amet sit amet",
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto rem quasi magnam possimus doloribus nobis neque non deserunt explicabo. Numquam autem sint quasi corporis quas iste provident iusto dolorem doloribus.Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    }
+]
+
+function headerSetting_dropdown(){
     //header
-
-
     $(".settingsWindow").data("active", "false");
     $("header .settings").click(function (e) { 
         let isActive =  $(".settingsWindow").data('active');
@@ -110,6 +191,8 @@ $(document).ready(function () {
         }
     });
 
+}
+function reminderAppMaxBTN(){
     //To Do app maximize app
     $(".heading .maximize").data("active", "false");
     $(".heading  .maximize").click(function (e) { 
@@ -138,82 +221,13 @@ $(document).ready(function () {
             
         }
     });
-
-
-    //Areticle panel settings drop down
-    $(".panel .settings").data("active", "false");
-    $(".panel .settings").click(function (e) { 
-        let isActive =  $(this).data('active');
-        e.preventDefault();
-        if(isActive  === "false"){
-            $(this).data("active", "true");
-            $("+.panelSettingsWindow",this).addClass("panelSettingsWindow-active");
-            $(this).css({"width":"20px","transform":"rotate(90deg)"});
-        }
-        else{
-            $(this).data("active", "false");
-            $("+.panelSettingsWindow",this).removeClass("panelSettingsWindow-active");
-            $(this).css({"width":"20px","transform":"rotate(0deg)"});
-            
-        }
-    });
-
-        //Top panel option drop down
-        $(".topPanelOption-heading").data("active", "false");
-        $(".topPanelOption-heading").click(function (e) { 
-            let isActive =  $(this).data('active');
-            e.preventDefault();
-            if(isActive  === "false"){
-                $(".topPanelOption-heading").data("active", "false");
-                $(this).data("active", "true");
-                $(".topPanelOption-list").removeClass("topPanelOption-list-active");
-                $("+.topPanelOption-list",this).addClass("topPanelOption-list-active");
-            }
-            else{
-                $(this).data("active", "false");
-                $("+.topPanelOption-list",this).removeClass("topPanelOption-list-active");
-                
-            }
-        });
-
-$(".leftPanelLabel").data("active", "false");
-$(".leftPanelLabel").click(function (e) { let isActive =  $(this).data('active');
-e.preventDefault();
-if(isActive  === "false"){
-    $(this).data("active", "true");
-    $("+.leftPanelOptions",this).addClass("showOptions");
 }
-else{
-    $(this).data("active", "false");
-    $("+.leftPanelOptions",this).removeClass("showOptions");
-    
+function topPanelDropdown(){   //Article panel settings drop down
+
+    addTopPanelOptions();
+
+
 }
-    
-});
-
-
-//////////////
-    var tasks=[{
-        taskId:1,
-        taskName:  "Portfolio Development",
-        status: "pending"
-    },{
-        taskId:2,
-        taskName:  "Easy Grocries",
-        status: "pending"
-    },{
-        taskId:3,
-        taskName:  "Resume",
-        status: "pending"
-    },{
-        taskId:4,
-        taskName:  "Regex Form",
-        status: "pending"
-    },{
-        taskId:5,
-        taskName:  "Wordpress",
-        status: "pending"
-    }];
 function taskList(){
     let taskContentHTML="";
 
@@ -242,7 +256,6 @@ function taskList(){
 
 ///
 }
-
 function deleteTask(){
     $(".taskDelete_button").click(function (e) { 
         e.preventDefault();
@@ -269,12 +282,129 @@ function addTask(){
         taskList(); //
     });
 }
+function addLeftPanelOptions(){
+    let content="";
+    leftPanel.forEach(element => {
+        content += `<div class="leftPanelOption">
+                        <div class="leftPanelLabel">
+                        <h1>${element.icon} ${element.name}</h1>
+                        </div>
+                        <div class="leftPanelOptions">`
+        element.options.forEach((option,i) => {
+            content +=  `<div class="leftPanelOption">${element.optionIcon[i]} ${option}</div>`
+        });
+        content +=      `</div>
+                    </div>`;
+    });
+    $(".panels.left").append(content);
+}
+function addTopPanelOptions(){
+    let content="";
+    topPanel.forEach(element => {
+        content+=`<div class="topPanelOption">
+        <h3 class="topPanelOption-heading">${element.name} <i class="dropdownIcon fas fa-caret-down"></i></h3>
+        <div class="topPanelOption-list">`
+        element.options.forEach((option,i) => {
+            content+= `<p class="topPanelOption-listBTN">${option}</p>`
+        });
+        content+=`</div></div>`;
+    });
+    $(".panels.top").append(content);
+}
+function panelFunctionality(){
+    $(".panel .settings").data("active", "false");
+    $(".panel .settings").click(function (e) { 
+        let isActive =  $(this).data('active');
+        e.preventDefault();
+        if(isActive  === "false"){
+            $(this).data("active", "true");
+            $("+.panelSettingsWindow",this).addClass("panelSettingsWindow-active");
+            $(this).css({"width":"20px","transform":"rotate(270deg)"});
+        }
+        else{
+            $(this).data("active", "false");
+            $("+.panelSettingsWindow",this).removeClass("panelSettingsWindow-active");
+            $(this).css({"width":"20px","transform":"rotate(180deg)"});
+            
+        }
+    });
 
+        //Top panel option drop down
+        $(".topPanelOption-heading").data("active", "false");
+        $(".topPanelOption-heading").mouseenter(function (e) { 
+            let isActive =  $(this).data('active');
+            e.preventDefault();
+            if(isActive  === "false"){
+                $(".topPanelOption-heading").data("active", "false");
+                $(this).data("active", "true");
+                $(".topPanelOption-list").removeClass("topPanelOption-list-active");
+                $("+.topPanelOption-list",this).addClass("topPanelOption-list-active");
+            }
+            else{
+                $(this).data("active", "false");
+                $("+.topPanelOption-list",this).removeClass("topPanelOption-list-active");
+                
+            }
+        });
+}
+function loadPostSettings(){
+    let content="";
+    postSettings.options.forEach(option => {
+        content+=
+            `
+            <p>${option.name} ${option.icon}</p>
+            `
+    });
+    $(".panelSettingsWindow").append(content);
+}
+function loadPosts(){
+    let content="";
+    posts.forEach(post => {
+        content+=
+            `
+                    <article>
+                        <div class="panel">
+                            <div class="settings"><i class="fas fa-ellipsis-v"></i></div>
+                            <div class="panelSettingsWindow"></div>
+                        </div>
+                        <h1 class="mainHeading">${post.heading}</h1>
+                        <h2 class="subHeading">${post.heading}</h2>
+                        <p class="description place">${post.location} <i class="fas fa-map-marker-alt"></i></p>
+                        <p class="description date">${post.date} <i class="fas fa-calendar-day"></i></p>
+                        <p class="content">${post.content}</p>
+                    </article>`
+    });
+    $(".articles").append(content);
+    loadPostSettings();
+}
 /////////////
+addLeftPanelOptions();
+reminderAppMaxBTN();
+headerSetting_dropdown();
+topPanelDropdown();
 taskList(); //
 deleteTask();
 addTask();
+
+loadPosts();
+panelFunctionality();
 //////////////
+
+$(".leftPanelLabel").data("active", "false");
+$(".leftPanelLabel").click(function (e) { let isActive =  $(this).data('active');
+e.preventDefault();
+if(isActive  === "false"){
+    $(this).data("active", "true");
+    $("+.leftPanelOptions",this).addClass("showOptions");
+}
+else{
+    $(this).data("active", "false");
+    $("+.leftPanelOptions",this).removeClass("showOptions");
+    
+}
+    
+});
+
 
         
 
